07a32b837f4218bb76a79db1cc9fe4b6c4f82d94
R-azy <muhammedrazy1798@gmail.com>
2025-03-12 09:48:20 +0530

    21&22

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage"; // Import directly
-import SignupPage from "./pages/SignupPage";
+import SignUpPage from "./pages/SignUpPage";
 import "./App.css";
 import Home from "./pages/Home";
 import CreateProduct from "./pages/createProduct";
@@ -9,19 +9,21 @@ import MyProducts from "./pages/myProducts";
 import Cart from "./pages/cart";
 import ProductDetails from "./pages/ProductDetails";
 import Profile from "./pages/profile";
+import CreateAddress from "./pages/createAddress";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/create-product/:id" element={<CreateProduct />} />
         <Route path="/create-product" element={<CreateProduct />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/my-products" element={<MyProducts />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/create-address" element={<CreateAddress />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,16 +1,18 @@
 import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
+import SignupPage from "./pages/SignUpPage";
 import Home from "./pages/Home";
 import CreateProduct from "./pages/createProduct";
 import MyProducts from "./pages/myProducts";
 import Cart from "./pages/cart";
 import Profile from "./pages/profile";
+import CreateAddress from "./pages/createAddress";
 export {
-    LoginPage,
-    SignupPage,
-    Home,
-    CreateProduct,
-    MyProducts,
-    Cart,
-    Profile,
-}
+  LoginPage,
+  SignupPage,
+  Home,
+  CreateProduct,
+  MyProducts,
+  Cart,
+  Profile,
+  CreateAddress,
+};
