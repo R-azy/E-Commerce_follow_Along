fead7b05d40635b835cea3fb1b1aa2f072b05037
R-azy <muhammedrazy1798@gmail.com>
2025-03-20 10:55:42 +0530

    25

--- backend/app.js
+++ backend/app.js
@@ -24,9 +24,11 @@ app.use('/products', express.static(path.join(__dirname, 'products')));
 
 const user = require("./controller/user");
 const product = require('./controller/product');
+const order = require('./controller/orders');
 
 app.use("/api/v2/user", user);
 app.use("/api/v2/product", product);
+app.use("/api/v2/orders", order);
 
 app.use(ErrorHandler);
 
